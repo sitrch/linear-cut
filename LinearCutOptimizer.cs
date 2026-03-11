6e0ad9321f6431eccc4ed44707425cc8dee876c4
sitrch <81875500+sitrch@users.noreply.github.com>
2026-03-11 13:20:58 +0300

    Create LinearCutOptimizer.cs

---
+++ LinearCutOptimizer.cs
@@ -0,0 +1,7 @@
+// LinearCutOptimizer.cs
+// Your implementation here
+namespace LinearCutOptimizerNamespace {
+    public class LinearCutOptimizer {
+        // Your methods and properties here
+    }
+}
