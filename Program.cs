496b20d80287c756f2926ff4c15ea7837a8ff311
sitrch <81875500+sitrch@users.noreply.github.com>
2026-03-11 13:44:22 +0300

    Create Program.cs file with entry point

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LinearCut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
