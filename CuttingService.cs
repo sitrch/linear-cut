7bb02a37959a9efaf3698a51b6314be6a61b1eed
sitrch <81875500+sitrch@users.noreply.github.com>
2026-03-12 19:02:58 +0300

    Add WPF MVVM UI with column role filter and CuttingService

---
+++ CuttingService.cs
@@ -0,0 +1,11 @@
+// CuttingService.cs
+
+using System;
+
+namespace LinearCutOptimization.Wpf
+{
+    public class CuttingService
+    {
+        // Implementation of cutting service logic
+    }
+}
