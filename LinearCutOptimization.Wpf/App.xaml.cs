0ebaecac79bfde125f64d6107863ab23cc4c092e
sitrch <81875500+sitrch@users.noreply.github.com>
2026-03-12 19:59:12 +0300

    Add WPF UI files and MVVM structure

---
+++ LinearCutOptimization.Wpf/App.xaml.cs
@@ -0,0 +1,10 @@
+// Basic App.xaml.cs for the WPF application
+using System;
+using System.Windows;
+
+namespace LinearCutOptimization.Wpf
+{
+    public partial class App : Application
+    {
+    }
+}
